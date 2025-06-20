3325029ffe2ede063c8c08ca4a61e7d09b7d6e86
Riyapun <168796227+Riyapun@users.noreply.github.com>
2025-06-20 17:13:14 +0530

    Add files via upload
    code for DataCollection ,Test

---
+++ testing.py
@@ -0,0 +1,9 @@
+#import cv2
+#import mediapipe as mp
+#import cvzone
+import tensorflow as tf
+print("sucessfuly install")
+print(tf.keras.models)
+'''print("cv2:", cv2.__version__)
+print("mediapipe:", mp.__version__)
+print("cvzone is working!")'''
